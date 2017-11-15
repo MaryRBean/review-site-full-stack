c0af33a4829c9fccff77a3712f9169100d861f08
Mary Bean <reilly.mary.m@gmail.com>
2017-11-14 20:38:03 -0500

    commit

--- src/main/java/reviewSiteFullStack/ReviewPopulator.java
+++ src/main/java/reviewSiteFullStack/ReviewPopulator.java
@@ -1,4 +1,5 @@
 package reviewSiteFullStack;
+
 import javax.annotation.Resource;
 
 import org.springframework.boot.CommandLineRunner;
@@ -6,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ReviewPopulator implements CommandLineRunner  {
+	
 	@Resource
 	private WeatherRepository weatherRepo;
 
@@ -31,13 +33,15 @@ public class ReviewPopulator implements CommandLineRunner  {
 		Food savory = new Food("Savory");
 		foodRepo.save(savory);
 
-		Country england = new Country(rainy, "England", savory, bland, sweet);
+		//String countryName, String img, String reviewCategory, String reviewContent, Weather weather, Food... countryFood
+		//(1993,"England", "/images/england.jpg", "Europe", "Temperate weather, rains every three days. Food can be bland, so use curry.");
+		Country england = new Country( "England", "/images/england.jpg", "Europe", "Temperate weather, rains every three days. Food can be bland, so use curry.",rainy, savory, bland, sweet);
 		countryRepo.save(england);
-		Country malaysia = new Country(hot, "Malaysia", spicy, savory);
+		Country malaysia = new Country("Malaysia", "/images/malaysia.jpg", "Asia", "Hot and humid, constant sweating, amazing mix of Indian, Chinese and Thai influence on the food.",hot, spicy, savory);
 		countryRepo.save(malaysia);
-		Country cambodia = new Country(hot, "Cambodia", savory, spicy);
+		Country cambodia = new Country("Cambodia", "/images/cambodia.jpg", "Asia", "Hot and humid. Lots of cute kids running the streets, the occasional monkey and horse.",hot, savory, spicy);
 		countryRepo.save(cambodia);
-		Country mexico = new Country(hot, "Mexico", spicy, savory);
+		Country mexico = new Country("Mexico","/images/mexico.jpg", "N.America", "Hot weather, amazing spicy food, watch out for cow tongue tacos. 10/10 recommend this country.",hot, spicy, savory);
 		countryRepo.save(mexico);
 	}
 }
